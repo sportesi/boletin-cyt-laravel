0078b67aad2a30ebd4bbfeabfe37a04a42e53ae6
Sebastian Portesi <sebastian.nahuel.portesi@gmail.com>
2017-10-19 19:18:02 -0300

    [Front] Agregar boton de crear noticia al lado del nombre #16

--- resources/views/frontend/news/edit.blade.php
+++ resources/views/frontend/news/edit.blade.php
@@ -73,6 +73,7 @@
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
                         <button type="submit" class="btn btn-primary">Crear Noticia</button>
+                        <a href="{{ route('news.index') }}" class="btn btn-default">Cancelar</a>
                     </div>
                 </div>
             </form>
