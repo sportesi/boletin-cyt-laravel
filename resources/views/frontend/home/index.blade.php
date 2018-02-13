85270c4be914760558d7aff692ec5f9de9caca2d
Sebastian Portesi <sportesi@octopus.com.ar>
2018-02-13 13:04:40 -0300

    fixed utf8decode for jumbotron

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -6,9 +6,10 @@
     <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark"
          style="background-image: url('{{ asset($latest->image_url) }}')">
         <div class="col-md-6 px-0 jumbo-caption">
-            <h1 class="jumbo-text display-4 font-italic">{{ str_limit($latest->title, 40) }}</h1>
-            <p class="jumbo-text lead my-3">{{ str_limit($latest->summary, 200) }}</p>
-            <p class="jumbo-text lead mb-0"><a href="#" class="text-white font-weight-bold">Leer Más...</a></p>
+            <h1 class="jumbo-text display-4 font-italic">{{ str_limit(utf8_decode($latest->title), 40) }}</h1>
+            <p class="jumbo-text lead my-3">{{ str_limit(utf8_decode($latest->summary), 200) }}</p>
+            <p class="jumbo-text lead mb-0"><a href="{{ route('news.show', [$latest->id]) }}"
+                                               class="text-white font-weight-bold">Leer Más...</a></p>
         </div>
     </div>
 @endsection
