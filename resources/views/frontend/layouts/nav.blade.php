e2316e3c87f1d6937bb059659a2995805094a8be
Sebastian Portesi <sportesi@octopus.com.ar>
2018-02-15 11:21:02 -0300

    Revert "Fix category name utf8 decoding and limit in navbar"
    This reverts commit 9f13d69

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -56,8 +56,7 @@
 <div class="nav-scroller py-1 mb-2">
     <nav class="nav d-flex justify-content-between">
         @foreach ($categories as $category)
-            <a class="p-2 text-muted"
-               href="{{ route('category', [$category->id]) }}">{{ str_limit(utf8_decode($category->name), $limit = 22, $end = '...') }}</a>
+            <a class="p-2 text-muted" href="{{ route('category', [$category->id]) }}">{{ $category->name }}</a>
         @endforeach
     </nav>
 </div>
