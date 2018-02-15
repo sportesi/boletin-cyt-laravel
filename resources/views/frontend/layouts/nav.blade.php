9f13d6974c3cfbbcc0910ff37f366931436f02c7
Sebastian Portesi <sportesi@octopus.com.ar>
2018-02-15 10:34:10 -0300

    Fix category name utf8 decoding and limit in navbar

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -56,7 +56,8 @@
 <div class="nav-scroller py-1 mb-2">
     <nav class="nav d-flex justify-content-between">
         @foreach ($categories as $category)
-            <a class="p-2 text-muted" href="{{ route('category', [$category->id]) }}">{{ $category->name }}</a>
+            <a class="p-2 text-muted"
+               href="{{ route('category', [$category->id]) }}">{{ str_limit(utf8_decode($category->name), $limit = 22, $end = '...') }}</a>
         @endforeach
     </nav>
 </div>
--- resources/views/frontend/layouts/sidebar.blade.php
+++ resources/views/frontend/layouts/sidebar.blade.php
@@ -26,7 +26,7 @@
         <div class="list-group">
             @foreach($categories as $category)
                 <a href="{{ route('category', ['id' => $category->id]) }}" class="list-group-item">
-                    {{ str_limit($category->name, $limit = 22, $end = '...') }}
+                    {{ str_limit(utf8_decode($category->name), $limit = 22, $end = '...') }}
                     <span class="badge">{{ $category->news_count }}</span>
                 </a>
             @endforeach
