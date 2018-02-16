a0fa54ac2dfdaae6981e7a5bfc0f25abfb279103
Sebastian Portesi <sportesi@octopus.com.ar>
2018-02-16 19:53:14 -0300

    New styles for nav buttons on frontend

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -7,37 +7,35 @@
         <div class="col-6 text-center">
             <a class="blog-header-logo text-dark" href="/">{{ config('app.name') }}</a>
         </div>
-        <div class="col-2 text-right">
+        <div class="col-3 text-right">
             @if(\Illuminate\Support\Facades\Auth::check())
-                <div class="text-center">
-                    <div class="dropdown">
-                        <button class="btn btn-sm btn-outline-secondary dropdown-toggle" type="button"
-                                id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false">
-                            {{ \Illuminate\Support\Facades\Auth::user()->name }}
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="{{ route('news.index') }}">
-                                Mis Noticias
-                            </a>
+                <a href="{{ route('news.create') }}" class="btn btn-outline-primary btn-sm pull-left">
+                    Nueva Noticia
+                </a>
+                &nbsp;
+                <div class="dropdown pull-right">
+                    <button class="btn btn-sm btn-outline-secondary dropdown-toggle" type="button"
+                            id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                        {{ \Illuminate\Support\Facades\Auth::user()->name }}
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="{{ route('news.index') }}">
+                            Mis Noticias
+                        </a>
 
-                            @if(\Illuminate\Support\Facades\Auth::user()->hasRole('admin'))
-                                <a class="dropdown-item" href="{{ route('bo.dash') }}">Panel de Control</a>
-                            @endif
+                        @if(\Illuminate\Support\Facades\Auth::user()->hasRole('admin'))
+                            <a class="dropdown-item" href="{{ route('bo.dash') }}">Panel de Control</a>
+                        @endif
 
-                            <a class="dropdown-item" href="#">
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                    {{ csrf_field() }}
-                                    <button type="submit" class="btn btn-sm btn-outline-secondary">Cerrar sesión
-                                    </button>
-                                </form>
-                            </a>
-                        </div>
+                        <a class="dropdown-item" href="#">
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-sm btn-outline-secondary">Cerrar sesión
+                                </button>
+                            </form>
+                        </a>
                     </div>
-                    <br>
-                    <a href="{{ route('news.create') }}" class="btn btn-primary btn-sm">
-                        <i class="fa fa-plus"></i> Crear Noticia
-                    </a>
                 </div>
             @else
                 <div class="dropdown dropleft">
